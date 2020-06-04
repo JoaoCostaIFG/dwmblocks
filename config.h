16b43e621b678978292896db2a403e1838c7843d
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-06-04 02:01:29 +0100

    Change: small formatting update in config file.

--- config.h
+++ config.h
@@ -1,14 +1,15 @@
 // Modify this file to change what commands output to your statusbar, and
 // recompile using the make command.
 
+/* name of the environment variable that will be set on button input */
 #define DWMBLOCKS_ENV "DWMBLOCKS_BUTTON"
 
 static const Block blocks[] = {
-  /* Icon   Command     Update Interval (s)   Update Signal */
-  { "",     "cpu",      120,                  1 },
-  { "",     "volume",   0,                    2 },
-  { "",     "clock",    30,                   3 },
-  { "♥",    "",         0,                    0 },
+  /*Icon*/  /*Command*/ /*Update Interval (s)*/ /*Update Signal*/
+  { "",       "cpu",      120,                    1 },
+  { "",       "volume",   0,                      2 },
+  { "",       "clock",    30,                     3 },
+  { "♥",      "",         0,                      0 },
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
